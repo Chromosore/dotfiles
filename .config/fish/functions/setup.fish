3744919dff4f0d64c333f1d23187112b793b852e
Chromosore <konrad.blanchard@gmail.com>
2021-11-01 18:35:49 +0100

    fish: Added setup script helper function
    At one moment, I got tired of doing `. ~/.config/fish/setup.fish` every
    time I wanted to test something in my setup script. So I added a setup
    function. That does extra things.
    
    Basically, there is the setup script and the setup function.
    The first one is tedious to invoke, you have to type in the whole path.
    The second is very easy to invoke, so in order to avoid mistakes (even
    though it will probably never happen) it prompts you when you call it
    after the first time.

---
+++ .config/fish/functions/setup.fish
@@ -0,0 +1,26 @@
+set -q XDG_DATA_HOME
+or set -l XDG_DATA_HOME ~/.local/share
+
+set -l SETUP_MARKER $XDG_DATA_HOME/fish/setup_done
+
+function setup -V SETUP_MARKER
+	if test -e "$SETUP_MARKER"
+		read -p "printf 'Setup already done. Do it again ? [y/N] '" answer
+
+		if test "$answer" != 'y' -a "$answser" != 'Y'
+			echo 'Abort.'
+			return 1
+		end
+	end
+
+	fish $__fish_config_dir/setup.fish $argv
+	set setup_status $status
+
+	if test $setup_status != 0
+		return $setup_status
+	else if test -z "$argv" || contains -- --help $argv
+		return $setup_status
+	else
+		touch $SETUP_MARKER
+	end
+end
