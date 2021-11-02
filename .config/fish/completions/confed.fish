0e643a19b591e4676f9a2e77eb912cca4d8aa583
Chromosore <konrad.blanchard@gmail.com>
2021-11-02 16:13:14 +0100

    fish: Improved `confed` completion
    Now, the command line options are completed and the file completion
    works on macOS.
    Before, the file completion made use of the -printf option of GNU find,
    which is not available in BSD find.
    
    Also, I refactored the two separate completions for regular and special
    config file into one function so there is now only one file completion.

--- .config/fish/completions/confed.fish
+++ .config/fish/completions/confed.fish
@@ -1,3 +1,20 @@
 complete -c confed -x
-complete -c confed -a '(for dir in $CONFED_PATH; find $dir/ -mindepth 1 -maxdepth 1 -printf "%f\n"; end)'
-complete -c confed -a '(for file in $CONFED_SPECIAL; echo (basename $file); end)'
+
+complete -c confed -s a -l create      -d 'Create a new config file'
+complete -c confed -s i -l interactive -d 'Prompt when multiples choices are available'
+complete -c confed -s s -l search      -d 'Only print the path of the configuration file'
+
+function __confed_list_config_files
+	for dir in $CONFED_PATH
+		find $dir/ -mindepth 1 -maxdepth 1 -print0 |
+			string split0 |
+			string replace -r '.*/' ''
+	end
+
+	for file in $CONFED_SPECIAL
+		echo (basename $file)
+	end
+end
+
+complete -c confed -n 'not __fish_seen_argument -s a -l create' \
+   -a '(__confed_list_config_files)'
