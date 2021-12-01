03208ac07917f24424951432dc76b5cbfa25e0e2
Chromosore <konrad.blanchard@gmail.com>
2021-12-01 18:26:31 +0100

    fish: function: Change mode_prompt
    The old mode prompt that displayed in the bottom of the terminal was
    undeniably cool, but it was causing too much issues, like lost
    scrollback lines.
    
    Also, the prompt flickered when it drew.

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,74 +1,29 @@
-function show
-	printf -- $argv > /dev/tty
-end
-
 function fish_mode_prompt --description 'Afficher le mode actuel'
-	show "\x1b[?25l"
-	clock
-
 	# Do nothing if not in vi mode
 	if test "$fish_key_bindings" = "fish_default_key_bindings"
 		return 0
 	end
 
-	type -q 'getCurPos'
-	and set -l cursorPos 'getCurPos'
-	or set -l cursorPos 'printf' '0;0'
-
-	set -l curpos ($cursorPos | string split ";")
-	test "$curpos" = ""
-	and set curpos[1] 0
-
-	test $curpos[1] -eq $LINES
-	and show "\x1b[S"
-
-	show "\x1b[s"
-	show "\x1b[$LINES;0H"
-
-	show (set_color -o yellow)
-	show "-- "
+	set_color -o
 
 	switch $fish_bind_mode
 		case default
-			show NORMAL
+			set_color red
+			printf N
 		case insert
-			show INSERT
+			set_color green
+			printf I
 		case replace_one
-			show REPLACE
+			set_color brred
+			printf r
 		case replace
-			show REPLACE
+			set_color brred
+			printf R
 		case visual
-			show VISUAL
-		end
+			set_color magenta
+			printf V
+	end
 
-	show " -- "
-
-	show "\x1b[u"
-
-	test $curpos[1] -eq $LINES
-	and show "\x1b[A"
-
-	show "\x1b[?25h"
-end
-
-function fish_clear_mode_prompt --on-event fish_preexec
-	#type -q 'getCurPos'
-	#and set -l cursorPos 'getCurPos'
-	#or set -l cursorPos 'printf' '0;0'
-
-	#set -l curpos ($cursorPos | string split ";")
-
-	#test $curpos[1] -ne $LINES
-	#and return
-
-	show "\x1b[s"
-
-	show "\x1b[$LINES;0H"
-	show "\x1b[K"
-
-	show "\x1b[u"
-end
-
-if set -q VIM
-	functions -e fish_mode_prompt
+	set_color normal
+	printf ' '
 end
