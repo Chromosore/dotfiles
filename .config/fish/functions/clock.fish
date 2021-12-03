27089428c4644e2f53ccfe7eb87d6b13bf39363a
Chromosore <konrad.blanchard@gmail.com>
2021-12-03 18:45:55 +0100

    fish: prompt: Fix the clock for the right prompt
    The clock used to display in the corner of the screen, but it doesn't
    play well with the right prompt, as it writes its output directly to the
    stdout, so fish cannot compute the clock string's length.
    
    Again, I forgot to commit this with the right prompt.

--- .config/fish/functions/clock.fish
+++ .config/fish/functions/clock.fish
@@ -3,14 +3,9 @@ function clock
 	set -l hcol (set_color $fish_color_cwd)
 	set -l normal (set_color normal)
 
-	show "\x1b[s"
-	show "\x1b[1;"(math $COLUMNS - 6)"H"
-
 	set -l time (date +%H:%M)
 
 	echo -sn $bcol [ $normal \
 		$hcol $time $normal \
-		$bcol ] $normal > /dev/tty
-
-	show "\x1b[u"
+		$bcol ] $normal
 end
