26a0d07f704901a49c6d6cc612adc6b946dbbc8e
Chromosore <konrad.blanchard@gmail.com>
2021-12-31 16:04:34 +0100

    fish: Fix CONFED_PATH in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,12 +15,18 @@ function _run_fish_hooks --on-variable PWD
 	fish_hooks
 end
 
+
+set -q XDG_CONFIG_DIR
+and set -l config $XDG_CONFIG_DIR
+or set -l config ~/.config
+
 # confed settings
 # confed is awesome
 # (I might be its creator)
 # (but whatever)
-set -gx CONFED_PATH $CONF/fish/functions $CONF/fish/completions
+set -gx CONFED_PATH $__fish_config_dir/functions $__fish_config_dir/completions
 set -a CONFED_PATH $HOME/.vim
 
-set CONFED_SPECIAL $HOME/.profile $HOME/.bashrc
-set -a CONFED_SPECIAL $CONF/mancolors
+set CONFED_SPECIAL $HOME/.profile
+set -a CONFED_SPECIAL $__fish_config_dir/setup.fish
+set -a CONFED_SPECIAL $config/mancolors
