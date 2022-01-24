e9544ea45614c53a67e0171f059d27826a04d2de
Chromosore <konrad.blanchard@gmail.com>
2022-01-24 15:13:54 +0100

    fish: Clean up prompt_postfix.fish

--- .config/fish/functions/prompt_postfix.fish
+++ .config/fish/functions/prompt_postfix.fish
@@ -1,19 +1,17 @@
-
-
 function prompt_postfix
-    set -q prompt_postfix_level
-    or set -l prompt_postfix_level ''
+	set -q prompt_postfix_level
+	or set -l prompt_postfix_level ''
 
-    set -q prompt_postfix
-    or set -l prompt_postfix '$'
+	set -q prompt_postfix
+	or set -l prompt_postfix '$ '
 
-    if test "$prompt_postfix_level_zero" = "1"
-	set prompt_level (math $SHLVL - 1)
-    else
-	set prompt_level $SHLVL
-    end
+	if test "$prompt_postfix_level_zero" = "1"
+		set prompt_level (math $SHLVL - 1)
+	else
+		set prompt_level $SHLVL
+	end
 
-    printf ' %s%s' \
-	(string repeat -n $prompt_level $prompt_postfix_level) \
-	$prompt_postfix
+	printf ' %s%s' \
+		(string repeat -n $prompt_level $prompt_postfix_level) \
+		$prompt_postfix
 end
