ee6dd9ab10e7f8b41f77ba161248fc40221b4f23
Chromosore <konrad.blanchard@gmail.com>
2022-02-23 14:47:23 +0100

    fish: Clean config.fish
    There are old snippets of configuration in my config.fish that I've
    removed on basically all my computers (because they are problematic or
    just useless), so let's just remove them in my dotfiles!
    
    Little description of what is removed:
      - `set -q PROFILE ... end` : it was causing infinite loops during
        startup on all of my system, since now none of them sets $PROFILE in
        ~/.profile
    
      - $LESSOPEN / $LESSCLOSE : `lesspipe` is just not installed on every
        platforms, and it is not *that* usefull
    
      - Some aliases : I don't like aliases in my config.fish and I don't
        use these aliases
    
      - Fish hooks : I've used them *once* and I don't use them anymore.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,3 @@
-if not set -q PROFILE
-	exec bash -c "source ~/.profile; exec fish"
-end
-
-if not set -qx LESSOPEN && not set -qx LESSCLOSE
-	lesspipe | source
-end
-
-alias info='info --vi-keys'
-alias ls='exa'
-alias bat='python3 -m rich.syntax -b default -t solarized-dark'
-
-fish_hooks
-function _run_fish_hooks --on-variable PWD
-	fish_hooks
-end
-
-
 set -q XDG_CONFIG_DIR
 and set -l config $XDG_CONFIG_DIR
 or set -l config ~/.config
