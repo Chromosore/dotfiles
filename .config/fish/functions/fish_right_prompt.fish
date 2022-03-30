4c345aff00067d04e96971b66fa6887878951521
Chromosore <konrad.blanchard@gmail.com>
2022-03-30 22:49:50 +0200

    fish: Make VCS status in right prompt asynchronous
    New feature! And hand-made! (I could've just used a plugin for that,
    but... where's the fun in this?)
    
    So the VCS status in now drawn *after* the prompt is drawn, which sounds
    a bit weird, because it is. The gist of it is that there is a first
    render of the prompt that notices that no CVS status has been rendered,
    so it schedules a render of this status. When the render is done -- this
    is detected with the --on-job-exit hook of functions -- a second render
    of the prompt is scheduled. In this render, the prompt has all of the
    resources it needs and the prompt is fully drawn.
    
    I also added a bit of caching onto that so there is no flicker at each
    new prompt. The flicker still occurs, though, when you leave a
    version-controlled directory.
    
    I really should've used a plugin for that.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,40 @@
+function __right_prompt_refresh_vcs_status
+	set -l tmpdir /tmp
+	set -q TMPDIR
+	and set -l tmpdir $TMPDIR
+
+	set tmpfile (mktemp $tmpdir/.fish_right_prompt_async.XXXXXXXXXX)
+
+	while true
+		set funcname __right_prompt_async_(random)
+		if not functions $funcname >/dev/null 2>/dev/null
+			break
+		end
+	end
+
+	fish -c 'fish_vcs_prompt' >$tmpfile &
+	set pid (jobs --last --pid)
+
+	function $funcname --on-job-exit $pid -V tmpfile -V funcname
+		set -g __right_prompt_vcs_status (cat $tmpfile)
+		command rm $tmpfile
+		commandline -f repaint
+		functions -e $funcname
+	end
+end
+
+
 function fish_right_prompt
 	prompt_ellapsed_time
-	fish_vcs_prompt
+
+	if set -g -q __right_prompt_vcs_status
+		echo -sn $__right_prompt_vcs_status
+		set -g __right_prompt_vcs_status_shadow $__right_prompt_vcs_status
+		set -g -e __right_prompt_vcs_status
+	else
+		echo -sn $__right_prompt_vcs_status_shadow
+		__right_prompt_refresh_vcs_status
+	end
 
 	printf ' '
 	clock
