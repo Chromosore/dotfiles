2f6b1b6acc4d6fa7ea36b2fa027429e15cf12655
Chromosore <konrad.blanchard@gmail.com>
2022-06-05 01:07:56 +0200

    nvim: Move `opt` plugins to `start` plugins
    The reason I'm doing that is that it's just *so much faster*. And it
    makes sense: `:packadd` has to go through all the `packpath` to find
    where the plugin lies. And this for every plugin!
    
    On the other hand, the `start` plugins go through the `packpath` only
    once and add all the plugins in each directory at the same time.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,10 @@
 _G.chromosore = require("chromosore")
 local pack = chromosore.pack
 
-
--- editing
-pack.add "vim-commentary"
-pack.add "vim-sandwich"
-
--- languages
-pack.add "nvim-lspconfig"
-pack.add "editorconfig.nvim"
-pack.add "nvim-treesitter"
-pack.add "spellsitter.nvim"
-pack.add "vim-quicktask"
-pack.add "vim-kitty"
-
--- neovim tooling
-pack.add "nvim-luapad"
-
--- ui
+-- conditionnaly loaded plugins
 if vim.g["chromosore#local#nerdfont"] == 1 then
 	pack.add "vim-devicons"
 end
-
-pack.add "fzf"
-pack.add "indent-blankline.nvim"
 
 -- lazily loaded plugins
 pack.lazy("nerdtree", {
